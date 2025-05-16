75e678600f28d88ee62f3ba1fe4082914fe0ccea
MalicknND <ndiayemalick062@gmail.com>
2025-05-16 16:51:52 +0200

    Mise à jour des listes de technologies dans le composant BentoGrid pour refléter les nouvelles compétences et outils.

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -53,8 +53,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "Typescript", "NodeJS"];
+  const rightLists = ["VueJS", "NextJS", "TailwindCSS"];
 
   const [copied, setCopied] = useState(false);
   const [isMounted, setIsMounted] = useState(false);
